d61214f90f00e144b39aa5f1720c2f2f5c58cbd8
jrosenbl <jay.m.rosenbloom@gmail.com>
2020-05-24 08:55:49 -0700

    lots of ugly hacks; backing up; refactoring coming

--- cov_state_ts.R
+++ cov_state_ts.R
@@ -20,6 +20,7 @@
 #' see also interactive map https://coronavirus.jhu.edu/map.html .
 #' 
 #' regarding last_update: So far, only one report file used m/d/y. The rest were Y-m-d.
+options(width=200)
 rm(list=ls())
 suppressMessages(library(data.table))
 suppressMessages(library(tidyr))
@@ -27,11 +28,12 @@ suppressMessages(library(ggplot2))
 suppressMessages(library(plotly))
 suppressMessages(library(htmlwidgets))
 suppressMessages(library(xts))
+
 #' ---------------
 #' customize these constants.
 kDataRoot <- "~/data/covid-19/COVID-19"
 kDataDir <- "csse_covid_19_data/csse_covid_19_daily_reports"
-kDefaultTargetState = 'New York'
+kDefaultTargetState = 'us'
 kOutputDir <- "~/data/covid19"
 kUSTotalOutput <- "us_totals.csv"
 kStartDate <- "2020-03-15"
@@ -72,7 +74,7 @@ plotData <- function(dt, targetState) {
         geom_bar(stat='identity') +
         geom_line(aes(report_date, dDelta), color='orange') +
         ylab('deaths') +
-        ggtitle(paste(targetState, format(st[nrow(st), report_date], "%Y-%m-%d")))
+        ggtitle(paste(targetState, format(dt[nrow(dt), report_date], "%Y-%m-%d")))
     targetState = gsub(" ","_", tolower(targetState))
     s = paste0(targetState, ".", kGraphDev)
     s <- file.path(kOutputDir, s)
@@ -99,10 +101,6 @@ if (length(reports) == 0) {
 latestReport <- reports[length(reports)]
 targetReport <- latestReport
 
-inPath <- file.path(kDataRoot, kDataDir, targetReport)
-if (!file.exists(inPath)) {
-    stop(inPath,' not found')
-}
 #'
 #' convert report list to an xts object for easy time indexing
 rd = sapply(strsplit(reports, "\\."), function(x) x[[1]])
@@ -122,60 +120,118 @@ for (r in rdi[dateRange]) {
     us[,report := r]
     combDt <- rbind(combDt, us)
 }
-#' get target state population
-stPop <- getStatePop(targetState)
 
-#' state totals over time
-st <- combDt[province_state == targetState, .(r=sum(recovered), c=sum(confirmed),d=sum(deaths)), by=report_date]
+if (targetState == "us") {
+    #' US totals over time
+    usTot <- combDt[report_date > startDate,.(r=sum(recovered), c=sum(confirmed), d=sum(deaths)), by=report_date]
 
-#' state deaths delta and percent change
-dDelta <- c(NA, diff(st$d, lag=1))
-st[, dDelta:=dDelta]
-dDeltaPct <- round(dDelta * 100 / myLag(st$d), 3)
-st[,dDeltaPct:=dDeltaPct]
+    #' US cases delta and percent change
+    delta <- c(NA, diff(usTot$c, lag=1))
+    usTot[,cDelta:=delta]
+    cDeltaPct <- round(delta * 100 / myLag(usTot$c), 3)
+    usTot[,cDeltaPct:=cDeltaPct]
 
-#' state deaths 5-day rolling mean of percent change
-dRollMean <- rollmean(dDeltaPct, kRollMeanDays, align="right", fill=NA)
-st[,dDeltaPctRollMean5d := dRollMean]
-cat("\n",targetState,"\n")
-st
+    #' US deaths delta and percent change
+    delta <- c(NA, diff(usTot$d, lag=1))
+    usTot[,dDelta:=delta]
+    dDeltaPct <- round(delta * 100 / myLag(usTot$d), 3)
+    usTot[,dDeltaPct:=dDeltaPct]
 
-#' US totals over time
-usTot <- combDt[report_date > startDate,.(r=sum(recovered), c=sum(confirmed), d=sum(deaths)), by=report_date]
+    #' US deaths 5-day rolling mean of percent change
+    dRollMean <- rollmean(dDeltaPct, 5, align="right", fill=NA)
+    usTot[,dDeltaPctRollMean5d := dRollMean]
+    cat("US\n")
+    #'
+    #' I think a bug is requiring []
+    print(usTot[])
+    
+    s = file.path(kOutputDir, kUSTotalOutput)
+    write.csv(usTot, file=s, quote=FALSE, row.names=FALSE)
+    cat(nrow(usTot), "lines saved in", s, "\n")
 
-#' US deaths delta and percent change
-delta <- c(NA, diff(usTot$d, lag=1))
-usTot[,dDelta:=delta]
-dDeltaPct <- round(delta * 100 / myLag(usTot$d), 3)
-usTot[,dDeltaPct:=dDeltaPct]
+    pUS <- plotData(usTot, "US")
+    s <- file.path(kOutputDir,paste0(gsub(" ","", "us"),".html"))
+    suppressWarnings(saveWidget(ggplotly(pUS), s, selfcontained=T))
+    cat("interactive plot saved in", s, "\n")
+    #'
+    #' average daily deaths
+    lst <- usTot[nrow(usTot)]$report_date
+    cat("average daily deaths over the last 14 days",round(mean(usTot[which(report_date==lst-14):which(report_date==lst)]$dDelta), 2),"\n")
+    #'
+    #' daily change in cases
+    outFile <- file.path(kOutputDir, paste0(fixNames(targetState), "_cases_",convDate(targetReport), ".", kGraphDev))
+    p = ggplot(usTot, aes(x=report_date, y=cDelta)) + geom_line()
+    ggsave(file=outFile, plot=p, device="png")
+    cat("daily cases graph saved in", outFile, "\n")
+    #'
+    #' daily change in deaths
+    outFile <- file.path(kOutputDir, paste0(fixNames(targetState), "_deaths_",convDate(targetReport), ".", kGraphDev))
+    p = ggplot(usTot, aes(x=report_date, y=dDelta)) + geom_line()
+    ggsave(file=outFile, plot=p, device="png")
+    cat("daily deaths graph saved in", outFile, "\n")
+} else {
+    #' get target state population
+    stPop <- getStatePop(targetState)
 
-#' US deaths 5-day rolling mean of percent change
-dRollMean <- rollmean(dDeltaPct, 5, align="right", fill=NA)
-usTot[,dDeltaPctRollMean5d := dRollMean]
-cat("\nUS\n")
-usTot
+    #' state totals over time
+    st <- combDt[province_state == targetState, .(r=sum(recovered), c=sum(confirmed),d=sum(deaths)), by=report_date]
 
-#' save xtab
-s = gsub(" ", "_", targetState)
-st[,state:=s]
+    #' state deaths delta and percent change
+    delta <- c(NA, diff(st$c, lag=1))
+    st[, cDelta:=delta]
+    cDeltaPct <- round(delta * 100 / myLag(st$c), 3)
+    st[,cDeltaPct:=cDeltaPct]
 
-s = file.path(kOutputDir, paste0(tolower(s),".csv"))
-write.csv(st, file=s, quote=FALSE, row.names=FALSE)
-cat(nrow(st), "lines saved in",s,"\n")
-s = file.path(kOutputDir, kUSTotalOutput)
-write.csv(usTot, file=s, quote=FALSE, row.names=FALSE)
-cat(nrow(usTot), "lines saved in", s, "\n")
+    #' state deaths delta and percent change
+    dDelta <- c(NA, diff(st$d, lag=1))
+    st[, dDelta:=dDelta]
+    dDeltaPct <- round(dDelta * 100 / myLag(st$d), 3)
+    st[,dDeltaPct:=dDeltaPct]
 
-#' plot state deaths and 
-#' plot state deaths and daily delta
-pState <- plotData(st, targetState)
-s <- file.path(kOutputDir,paste0(gsub(" ","", tolower(targetState)),".html"))
-suppressWarnings(saveWidget(ggplotly(pState), s, selfcontained=T))
-cat("interactive plot saved in", s, "\n")
+    #' state deaths 5-day rolling mean of percent change
+    dRollMean <- rollmean(dDeltaPct, kRollMeanDays, align="right", fill=NA)
+    st[,dDeltaPctRollMean5d := dRollMean]
+    cat("\n",targetState,"\n")
+    print(st[])
 
-pUS <- plotData(usTot, "US")
-s <- file.path(kOutputDir,paste0(gsub(" ","", "us"),".html"))
-suppressWarnings(saveWidget(ggplotly(pUS), s, selfcontained=T))
-cat("interactive plot saved in", s, "\n")
-cat(targetState,'state population US Census 2019 Estimate', format(stPop,big.mark=','), "\n")
-cat('deaths per 100K', st[nrow(st)]$d/(stPop/1e5),'\n')
+    #' save xtab
+    s = gsub(" ", "_", targetState)
+    st[,state:=s]
+
+    s = file.path(kOutputDir, paste0(tolower(s),".csv"))
+    write.csv(st, file=s, quote=FALSE, row.names=FALSE)
+    cat(nrow(st), "lines saved in",s,"\n")
+
+    #' plot state deaths and 
+    #' plot state deaths and daily delta
+    pState <- plotData(st, targetState)
+    s <- file.path(kOutputDir,paste0(gsub(" ","", tolower(targetState)),".html"))
+    suppressWarnings(saveWidget(ggplotly(pState), s, selfcontained=T))
+    cat("interactive plot saved in", s, "\n")
+
+    cat(targetState,'state population US Census 2019 Estimate', format(stPop,big.mark=','), "\n")
+    cat('deaths per 100K', st[nrow(st)]$d/(stPop/1e5),'\n')
+    lst <- st[nrow(st)]$report_date
+    cat("average daily deaths over the last 14 days",round(mean(st[which(report_date==lst-14):which(report_date==lst)]$dDelta), 2),"\n")
+    #'
+    #' day of week histogram
+    st[,wd:= factor(weekdays(report_date), levels=c('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'))]
+    pWd = ggplot(st, aes(x = wd, y=dDelta)) + geom_bar(stat = 'identity')
+    outFile <- paste0(fixNames(targetState), "_wd_",convDate(targetReport), ".", kGraphDev)
+    outFile <- file.path(kOutputDir, outFile)
+    suppressWarnings(ggsave(plot=pWd, file=outFile, device=kGraphDev))
+    cat("weekday plot saved in ", outFile, "\n")
+    #'
+    #' daily change in cases
+    outFile <- file.path(kOutputDir, paste0(fixNames(targetState), "_cases_",convDate(targetReport), ".", kGraphDev))
+    p = ggplot(st, aes(x=report_date, y=cDelta)) + geom_line()
+    ggsave(file=outFile, plot=p, device="png")
+    cat("daily cases graph saved in", outFile, "\n")
+    #'
+    #' daily change in deaths
+    outFile <- file.path(kOutputDir, paste0(fixNames(targetState), "_deaths_",convDate(targetReport), ".", kGraphDev))
+    p = ggplot(st, aes(x=report_date, y=dDelta)) + geom_line()
+    ggsave(file=outFile, plot=p, device="png")
+    cat("daily deaths graph saved in", outFile, "\n")
+
+}
